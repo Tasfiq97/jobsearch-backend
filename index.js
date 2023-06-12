751a81afbae98d98e0dc38975daf28c835460181
tasfiq97 <tasfiqulazad200@gmail.com>
2023-06-13 01:38:29 +0600

    first commit 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const port = 5000;
+const port = process.env.PORT;
 require("dotenv").config();
 
 app.use(cors());
